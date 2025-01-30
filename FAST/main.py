0760eba07329e79f50f9c01fe384998edbc8346f
yahircj <130490177+yahircj@users.noreply.github.com>
2025-01-29 22:11:01 -0600

    Practica 2, avance?

--- FAST/main.py
+++ FAST/main.py
@@ -7,3 +7,16 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {'hello':'world FastAPI'}
+
+#endpoit promedio
+@app.get('/promedio')
+def promedio():
+    return 10.5
+
+
+#endpoit parametros obligatorios
+@app.get('/usuario/{id}')
+def consultorio(id:int):
+    #caso ficticio de busqueda
+    return {'Se encontro al usuario:':id}
+    
