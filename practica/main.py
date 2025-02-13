294c34cfe75c60f4c03a8bba5c0d565ce03e790e
yahircj <130490177+yahircj@users.noreply.github.com>
2025-02-13 16:48:51 -0600

    practica 4
    lista de tareas

--- practica/main.py
+++ practica/main.py
@@ -5,3 +5,10 @@ app = FastAPI(
     title='Practica de api',
     description= 'Jonathan Yahir Contreras'
 )
+
+Tareas = [
+{"id": 1,"titulo": "Estudiar para el examen","descripcion": "Repasar los apuntes de TAI ","vencimiento": "14-02-24", "Estado": "incompleta"},
+{"id": 2,"titulo": "Autodidacta","descripcion": "Dejar de procastinar ","vencimiento": "16-02-24", "Estado": "completada"},
+{"id": 3,"titulo": "Acabar videojuegos","descripcion": "Pasar la campana de death craft ll","vencimiento": "20-02-24", "Estado": "incompleta"},
+{"id": 4,"titulo": "Diseñar","descripcion": "Ver tutoriales de render","vencimiento": "31-02-24", "Estado": "completada"}
+]
