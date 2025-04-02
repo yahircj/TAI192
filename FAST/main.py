3162754e0ce8255f4b7a843299b4dbba5e1dba84
yahircj <130490177+yahircj@users.noreply.github.com>
2025-04-02 15:15:33 -0600

    Importacion de middleware cors

--- FAST/main.py
+++ FAST/main.py
@@ -3,12 +3,21 @@ from db.conexion import engine,Base
 from routers.usuario import routerUsuario
 from routers.auth import routerAuth
 
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title='mi primer api',
     description= 'Jonathan Yahir Contreras',
     version='1.0.1'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(routerUsuario)
